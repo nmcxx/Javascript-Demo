228fcf353c6341b3f5a9063392a22bfefc2a724f
Nhữ Mạnh Cần <maccervn@gmail.com>
2021-07-06 16:31:45 +0700

    todo function completed

---
+++ Todo/js/todo.js
@@ -0,0 +1,80 @@
+var input = document.getElementById("txtTodoName");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      let text = input.value.trim();
+      if(text=='')
+      {
+        return;
+      }
+
+      Todo(text);
+      console.log(text);
+
+      input.value = '';
+      input.focus();
+    }
+  });
+
+  let todoItems = [];
+
+  function Todo(text){
+    const todo = {
+      text,
+      checked: false,
+      id: Date.now()
+    };
+
+    todoItems.push(todo);
+    saveStorage();
+    loadTodo(todo);
+  }
+
+  function loadTodo(todo)
+  {
+    // localStorage.setItem('todoItems', JSON.stringify(todoItems))
+
+    let li = document.createElement("li");
+    // li.setAttribute("id", todo.id);
+    li.id = todo.id;
+    document.getElementById("todoList").appendChild(li);
+
+    let checked = todo.checked == true ? "checked" : "";
+
+    li.innerHTML = `
+    <input id="${todo.id}" type="checkbox" ${checked}>
+    <span>${todo.text}</span>
+    <button onclick="deleteTodo(${todo.id})">Xoá</button>`;
+  }
+
+  function deleteTodo(id)
+  {
+      let todo = document.getElementById(id);
+      todo.parentNode.removeChild(todo);
+
+      todoItems = todoItems.filter(item => item.id !== Number(id));
+      saveStorage();
+  }
+
+  function saveStorage()
+  {
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+  }
+
+  function loadStorage()
+  {
+    return localStorage.getItem('todoItems');
+  }
+
+  document.addEventListener('DOMContentLoaded', () =>{
+    const ref = loadStorage();
+    if(ref)
+    {
+      todoItems = JSON.parse(ref);
+      todoItems.forEach(element => {
+        loadTodo(element);
+      });
+    }
+  });
+
+
+
